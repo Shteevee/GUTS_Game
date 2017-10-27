7970a12fb39cf6927d797dd8382828b081a2f2dc
Ewan <ewan.corbett@yahoo.co.uk>
2017-10-27 21:36:05 +0100

    Car started and empty screen

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from sys import *
-from pygame import *
+import pygame
+
+running = True
+while (running):
+
+    window = pygame.display.init()
+    pygame.display.set_mode((900,640))
+
+    event = pygame.event.wait ()
+    if event.type == pygame.QUIT:
+        running = False
+
+pygame.quit()
