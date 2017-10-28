28168565579147679f883bf89f4cfa857a70be2d
Ewan <ewan.corbett@yahoo.co.uk>
2017-10-28 17:48:00 +0100

    Camera works now

--- camera.py
+++ camera.py
@@ -1,14 +1,23 @@
 import pygame, sys, math
 from pygame.locals import *
 
+def simple_camera(camera, target_rect):
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    return Rect(-l + HALF_WIDTH, -t + HALF_HEIGHT, w, h)
+
 class Camera(object):
-    def __init__(self, camera_func, width, height):
-        self.camera_func = camera_func
+    def __init__(self, width, height):
+        self.camera_func = simple_camera
         self.state = Rect(0,0,width, height)
+        self.HALF_WIDTH = int(width/2.75)
+        self.HALF_HEIGHT = int(height/5)
 
     def apply(self, target):
         return target.rect.move(self.state.topleft)
 
     def update(self, target):
-        self.state = self.camera_func(self.state, target.rect)
+        self.state.left = -target.rect.left + self.HALF_WIDTH
+        self.state.top = -target.rect.top + self.HALF_HEIGHT
+        # self.state = self.camera_func(self.state, target.rect)
         print (self.state)
--- main.py
+++ main.py
@@ -2,24 +2,20 @@ import pygame, sys, gameMap, car, camera
 from pygame.locals import *
 import settings
 
-def simple_camera(camera, target_rect):
-    l, t, _, _ = target_rect
-    _, _, w, h = camera
-    return Rect(-l+HALF_WIDTH, -t+HALF_HEIGHT, w, h)
+
 
 clock = pygame.time.Clock()
 
 WIN_WIDTH = 900
 WIN_HEIGHT = 640
-HALF_WIDTH = int(WIN_WIDTH/2)
-HALF_HEIGHT = int(WIN_HEIGHT/2)
+
 
 display = pygame.display
 display.init()
 screen = display.set_mode((WIN_WIDTH,WIN_HEIGHT))
 car = car.Car(5200, 4800)
 bg = gameMap.GameMap()
-camera = camera.Camera(simple_camera, WIN_WIDTH, WIN_HEIGHT)
+camera = camera.Camera(WIN_WIDTH, WIN_HEIGHT)
 entities = pygame.sprite.Group()
 entities.add(bg)
 entities.add(car)
@@ -28,10 +24,16 @@ running = True
 while (running):
     clock.tick(30)
 
-    # car = car.Car(10,10)
-    # bg = gameMap.GameMap()
-    running = True
-    clock = pygame.time.Clock()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if not hasattr(event,'key'): continue
+        down = event.type == KEYDOWN
+        #CHANGE THESE SCALARS TO CHANGE ACCELERATIONS AND ROTATION SPEED
+        if event.key==K_RIGHT: car.updateRight(down * -3)
+        elif event.key==K_LEFT: car.updateLeft(down * 3)
+        elif event.key==K_UP: car.updateUp(down * 0.5)
+        elif event.key==K_DOWN: car.updateDown(down * -0.3)
 
     car.drive(bg)
     print(car.getPos())
